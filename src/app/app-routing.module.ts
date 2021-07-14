e306ff69d3ccc8453a1bbbd56316542c041e30a0
viswadha24 <viswadha24@gmail.com>
2021-07-15 00:40:01 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TestimonalsComponent } from './testimonals/testimonals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"testimonals",component:TestimonalsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
