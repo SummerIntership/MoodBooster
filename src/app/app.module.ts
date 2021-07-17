f49dcb80f1fc6eca16f86f96752334e442ac561e
SholinghurPooja <80974024+SholinghurPooja@users.noreply.github.com>
2021-07-18 00:12:16 +0530

    analysis component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TestimonalsComponent } from './testimonals/testimonals.component';
-<<<<<<< HEAD
 import { AnalysisComponent } from './analysis/analysis.component';
 import { TrackComponent } from './track/track.component';
 import { PerformanceComponent } from './performance/performance.component';
-=======
 import { DefaultComponent } from './default/default.component';
->>>>>>> 29fd831012b37895727cbd8792fa84dd08a277e6
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     TestimonalsComponent,
-<<<<<<< HEAD
     AnalysisComponent,
     TrackComponent,
-    PerformanceComponent
-=======
+    PerformanceComponent,
     DefaultComponent
->>>>>>> 29fd831012b37895727cbd8792fa84dd08a277e6
   ],
   imports: [
     BrowserModule,
